cfb12c93296ea0d2d996eb3099e5c255db63660d
Raj Lathigra <63877323+raj-1106@users.noreply.github.com>
2024-11-19 21:22:28 +0530

    removed Contact page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
